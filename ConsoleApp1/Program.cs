e2470583e03b42d9309c817257b63595ee9b34d9
Gelller <geller.ggs@gmail.com>
2020-12-09 12:35:51 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Введите имя");
+        string name = Console.ReadLine();
+        Console.WriteLine("Привет, {0}, сегодня {1}.", name ,DateTime.Now);
+        Console.ReadLine();
+        }
+    }
+}
